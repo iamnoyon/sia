d4c3a4219d16cb7d7da2ec93023e01e12422f7a4
iamnoyon <mdmahfujarrahmannoyon09@gmail.com>
2022-09-22 10:11:03 +0600

    added database classes

--- alchemy/tables.py
+++ alchemy/tables.py
@@ -11,18 +11,54 @@ class Members(Base):
     __table_args__ = {'mysql_charset': 'utf8mb4', 'mysql_collate': 'utf8mb4_bin'}
 
     id = Column(INTEGER, primary_key=True, autoincrement=True)
-    internal_id = Column(INTEGER, unique=True, nullable=False)
+    #internal_id = Column(INTEGER, unique=True, nullable=False)
     url = Column(VARCHAR(350))
+    language = Column(VARCHAR(2))
+    full_address = Column(VARCHAR(350))
+    gender = Column(MEDIUMTEXT)
+    name = Column(MEDIUMTEXT)
+    education = Column(MEDIUMTEXT)
+    address = Column(MEDIUMTEXT)
+    city = Column(MEDIUMTEXT)
+    zipcode = Column(MEDIUMTEXT)
+    email = Column(MEDIUMTEXT)
+    tel = Column(MEDIUMTEXT)
+    fax = Column(MEDIUMTEXT)
+    website = Column(MEDIUMTEXT)
+    job = Column(MEDIUMTEXT)
+    sector = Column(MEDIUMTEXT)
+    group = Column(MEDIUMTEXT)
+    section = Column(MEDIUMTEXT)
 
-    publication_time = Column(DATETIME)
-    has_video = Column(BOOLEAN)
-    category = Column(MEDIUMTEXT)
-    subcategory = Column(MEDIUMTEXT)
-    title = Column(MEDIUMTEXT)
-    subtitle = Column(MEDIUMTEXT)
-    text = Column(LONGTEXT)
-    main_image_url = Column(MEDIUMTEXT)
+class Offices(Base):
 
+    __tablename__ = 'office'
+    __table_args__ = {'mysql_charset': 'utf8mb4', 'mysql_collate': 'utf8mb4_bin'}
+
+    id = Column(INTEGER, primary_key=True, autoincrement=True)
+    #internal_id = Column(INTEGER, unique=True, nullable=False)
+    url = Column(VARCHAR(350))
+    language = Column(VARCHAR(2))
+    full_address = Column(VARCHAR(350))
+    name = Column(MEDIUMTEXT)
+    address = Column(MEDIUMTEXT)
+    city = Column(MEDIUMTEXT)
+    zipcode = Column(MEDIUMTEXT)
+    email = Column(MEDIUMTEXT)
+    tel = Column(MEDIUMTEXT)
+    fax = Column(MEDIUMTEXT)
+    website = Column(MEDIUMTEXT)
+    sector = Column(MEDIUMTEXT)
+
+class MemberOffice(Base):
+
+    __tablename__ = 'member-office'
+    __table_args__ = {'mysql_charset': 'utf8mb4', 'mysql_collate': 'utf8mb4_bin'}
+
+    id = Column(INTEGER, primary_key=True, autoincrement=True)
+    member_id = Column(INTEGER, unique=True, nullable=True)
+    office_id = Column(INTEGER, unique=True, nullable=True)
+    created_date = Column(DATETIME)
 
 def create_all(engine):
     print("creating databases")
